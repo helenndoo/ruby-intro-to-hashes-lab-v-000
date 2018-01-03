b505acbc9b1d71536a3caf2fb0661ac10507e86b
Helen Du <helendu.614@gmail.com>
2018-01-03 17:41:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 					:two_pieces_owned=> 50,
 					:three_pieces_owned=> 100,
 					:four_pieces_owned=> 200,
-				},
+				}
 			}
 		}
 end
@@ -68,7 +68,7 @@ def monopoly_with_fourth_tier
 				:two_pieces_owned=> 50,
 				:three_pieces_owned=> 100,
 				:four_pieces_owned=> 200,
-			},
+			}
 		}
 	}
 end
